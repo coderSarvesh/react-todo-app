cc24da91fa9e997e0cf049af3131e9a4b1f3be82
rane_sarvesh <ranesarvesh97@gmail.com>
2023-01-11 20:24:59 +0530

    css changes 2

--- src/Todo.js
+++ src/Todo.js
@@ -28,14 +28,14 @@ const Todo = () => {
             <div className=''>
                 <h1 className='m-10 text-3xl'>To-Do List</h1>
                 <div className='flex flex-col items-center justify-center'>
-                    <div className='flex border-2 text-xl border-gray-400' >
-                        <input className='px-6 py-4 text-xl border-gray-400' type="text"
+                    <div className='flex border-2 text-xl w-screen border-gray-400 md:w-1/3' >
+                        <input className='px-6 py-4 text-xl border-gray-400 w-[80%]' type="text"
                             placeholder="Enter A Todo"
                             value={input}
                             onChange={(e) => setInput(e.target.value)}
                         />
-                        <button onClick={saveData} >
-                            <FaPlus className="m-5" />
+                        <button className='m-[8%]' onClick={saveData} >
+                            <FaPlus className="w-5" />
                         </button>
                     </div>
 
@@ -43,10 +43,10 @@ const Todo = () => {
                         {
                             todolist.map((item, id) => {
                                 return (
-                                    <div className='flex mt-4 py-2 px-9 bg-slate-300' key={id}>
+                                    <div className='flex justify-between mt-4 mx-4 w-screen md:w-screen md:mx-8 bg-slate-300' key={id}>
                                         <h1 className='text-2xl font-medium'>{item}</h1>
-                                        <button onClick={() => deleteTodo(id)}>
-                                            <FaTimes className="ml-36 m-1 text-xl" />
+                                        <button className='ml-28 mr-0' onClick={() => deleteTodo(id)}>
+                                            <FaTimes className="m-1 text-xl" />
                                         </button>
                                     </div>
                                 )
